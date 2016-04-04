7e9b1ab5aeb8c0eb7c434dfa15d054a5387f97ad
T.Kabu <kabu@fvg-on.net>
2016-04-04 13:26:15 +0900

    Update ban4ipd_unban.php

--- ban4ipd_unban.php
+++ ban4ipd_unban.php
@@ -12,91 +12,6 @@
 // ----------------------------------------------------------------------
 // Sub Routine
 // ----------------------------------------------------------------------
-function ban4ip_unban_old($TARGET_CONF)
-{
-    // 対象IPアドレスがIPv6なら(IPv6だったら文字列そのものが返ってくる)
-    if (filter_var($TARGET_CONF['target_address'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== FALSE)
-    {
-        // 対象サービスについてBANの設定があるなら
-        if (isset($TARGET_CONF['target_port']) && isset($TARGET_CONF['target_rule']))
-        {
-            // -----------------------------
-            // ip6tablesから該当ルールを削除する
-            // -----------------------------
-            // ban4ipチェインの設定を取得する
-            $PROC_P = popen($TARGET_CONF['ip6tables']." -L ban4ip -n", "r");
-            $TARGET_PATTERN = '/^'.$TARGET_CONF['target_rule'].' .* '.$TARGET_CONF['target_address'].'\/.* dpt:'.$TARGET_CONF['target_port'].' /';
-            // ban4ipチェインに該当ルールがあるなら
-            if (psearch($PROC_P, $TARGET_PATTERN) == TRUE)
-            {
-                // ip6tablesのban4ipチェインから対象IPアドレスについて削除する
-                system($TARGET_CONF['ip6tables'].' -D ban4ip --source '.$TARGET_CONF['target_address'].' --proto tcp --dport '.$TARGET_CONF['target_port'].' --jump '.$TARGET_CONF['target_rule']);
-                // 対象IPアドレスをUNBANした旨を出力
-                $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unban ".$TARGET_CONF['target_address']."\n";
-            }
-            else
-            {
-                // 対象IPアドレスがUNBANされている旨を出力
-                $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unbaned aleady ".$TARGET_CONF['target_address']."\n";
-            }
-            pclose($PROC_P);
-        }
-        else
-        {
-            // 対象IPアドレスをUNBAN?した旨を出力
-            $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unban? ".$TARGET_CONF['target_address']."\n";
-        }
-    }
-    // 対象IPアドレスがIPv4なら(IPv4だったら文字列そのものが返ってくる)
-    else if (filter_var($TARGET_CONF['target_address'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== FALSE)
-    {
-        // 対象サービスについてBANの設定があるなら
-        if (isset($TARGET_CONF['target_port']) && isset($TARGET_CONF['target_rule']))
-        {
-            // -----------------------------
-            // iptablesから該当ルールを削除する
-            // -----------------------------
-            // ban4ipチェインの設定を取得する
-            $PROC_P = popen($TARGET_CONF['iptables']." -L ban4ip -n", "r");
-            $TARGET_PATTERN = '/^'.$TARGET_CONF['target_rule'].' .* '.$TARGET_CONF['target_address'].' .* dpt:'.$TARGET_CONF['target_port'].' /';
-            // ban4ipチェインに該当ルールがあるなら
-            if (psearch($PROC_P, $TARGET_PATTERN) == TRUE)
-            {
-                // iptablesのban4ipチェインに対象IPアドレスについて追加する
-                system($TARGET_CONF['iptables'].' -D ban4ip --source '.$TARGET_CONF['target_address'].' --proto tcp --dport '.$TARGET_CONF['target_port'].' --jump '.$TARGET_CONF['target_rule']);
-                // 対象IPアドレスをUNBANした旨を出力
-                $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unban ".$TARGET_CONF['target_address']."\n";
-            }
-            else
-            {
-                // 対象IPアドレスがUNBANされている旨を出力
-                $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unbaned aleady ".$TARGET_CONF['target_address']."\n";
-            }
-            pclose($PROC_P);
-        }
-        else
-        {
-            // 対象IPアドレスをUNBAN?した旨を出力
-            $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unban? ".$TARGET_CONF['target_address']."\n";
-        }
-    }
-    // 対象IPアドレスがIPv4でもIPv6でもないなら
-    else
-    {
-        // 対象IPアドレスはBANの対象だけど、アドレスがおかしい旨のメッセージを設定
-        $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Ban ".$TARGET_CONF['target_address']." (over ".$TARGET_CONF['maxretry']." counts) ";
-        $TARGET_CONF['log_msg'] .= 'Illegal address!?'."\n";
-    }
-    // BANデータベースから対象IPアドレス(とポートとルールが合致するもの)を削除
-    $TARGET_CONF['ban_db']->exec("DELETE FROM ban_tbl WHERE address = '".$TARGET_CONF['target_address']."' AND port = '".$TARGET_CONF['target_port']."' AND rule = '".$TARGET_CONF['target_rule']."'");
-    // 戻る
-    return $TARGET_CONF;
-}
-?>
-<?php
-// ----------------------------------------------------------------------
-// Sub Routine
-// ----------------------------------------------------------------------
 function ban4ip_unban($TARGET_CONF)
 {
     // 対象IPアドレスがIPv6なら(IPv6だったら文字列そのものが返ってくる)
@@ -130,6 +45,9 @@ function ban4ip_unban($TARGET_CONF)
             // -----------------------------
             // ip6tablesに対象IPアドレスをBANするルールを設定する
             // -----------------------------
+            // UNBANする前のコマンド(exec_befor_unban)が設定されていたら実行(UNBANする場合、すでにUNBAN済みでも実行)
+            $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_befor_unban');
+            
             // ban4ipチェインの設定を取得する
             $PROC_P = popen($IPTABLES." -L ban4ip -n", "r");
             $TARGET_PATTERN = '/^'.$TARGET_CONF['target_rule'].' .* '.$TARGET_CONF['target_address'].'.* $/';
@@ -147,6 +65,9 @@ function ban4ip_unban($TARGET_CONF)
                 $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unbaned aleady ".$TARGET_CONF['target_address']."\n";
             }
             pclose($PROC_P);
+            
+            // UNBANした後のコマンド(exec_afer_unban)が設定されていたら実行(UNBANする場合、すでにUNBAN済みでも実行)
+            $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_after_unban');
         }
         // 対象サービスについてBANのプロトコルとポートが個別に設定されているなら
         else if (isset($TARGET_CONF['target_protcol']) && isset($TARGET_CONF['target_port']))
@@ -154,6 +75,9 @@ function ban4ip_unban($TARGET_CONF)
             // -----------------------------
             // ip6tablesに対象IPアドレスをBANするルールを設定する
             // -----------------------------
+            // UNBANする前のコマンド(exec_befor_unban)が設定されていたら実行(UNBANする場合、すでにUNBAN済みでも実行)
+            $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_befor_unban');
+            
             // ban4ipチェインの設定を取得する
             $PROC_P = popen($IPTABLES." -L ban4ip -n", "r");
             $TARGET_PATTERN = '/^'.$TARGET_CONF['target_rule'].' .* '.$TARGET_CONF['target_address'].'.* '.$TARGET_CONF['target_protcol'].' dpt:'.$TARGET_CONF['target_port'].' $/';
@@ -171,6 +95,9 @@ function ban4ip_unban($TARGET_CONF)
                 $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s")." ban4ip[".getmypid()."]: NOTICE [".$TARGET_CONF['target_service']."] Unbaned aleady ".$TARGET_CONF['target_address']."\n";
             }
             pclose($PROC_P);
+            
+            // UNBANした後のコマンド(exec_afer_unban)が設定されていたら実行(UNBANする場合、すでにUNBAN済みでも実行)
+            $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_after_unban');
         }
         else
         {
