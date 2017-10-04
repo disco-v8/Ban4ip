aa2d520968e26dfa3c4dff9af5728fcf535dc280
T.Kabu <kabu@fvg-on.net>
2017-10-04 11:39:18 +0900

    Update ban4ipd_ban.php

--- ban4ipd_ban.php
+++ ban4ipd_ban.php
@@ -15,6 +15,7 @@
 function ban4ip_sendmsg($TARGET_CONF)
 {
     $FROM_SOCKET = '';
+    $SEND_MSG = date("Y-m-d H:i:s", local_time())." ban4ip[".getmypid()."]: ".$TARGET_CONF['log_msg'];
     
     // UNIXソケットが開いていなかったら
     if (!isset($TARGET_CONF['socket']) || !is_resource($TARGET_CONF['socket']))
@@ -25,7 +26,7 @@ function ban4ip_sendmsg($TARGET_CONF)
         return;
     }
     // UNIXソケット経由でメッセージを親プロセスに送信
-    $SOCK_RESULT = socket_send($TARGET_CONF['socket'], $TARGET_CONF['log_msg'], strlen($TARGET_CONF['log_msg']), 0);
+    $SOCK_RESULT = socket_send($TARGET_CONF['socket'], $SEND_MSG, strlen($SEND_MSG), 0);
     // もし送信できなかったら
     if ($SOCK_RESULT === FALSE)
     {
@@ -35,7 +36,7 @@ function ban4ip_sendmsg($TARGET_CONF)
         return;
     }
     // もしすべてを送信できなかったら
-    else if ($SOCK_RESULT != strlen($TARGET_CONF['log_msg']))
+    else if ($SOCK_RESULT != strlen($SEND_MSG))
     {
         // エラーメッセージに、親プロセスに全てを送信できなかった旨を設定
         print date("Y-m-d H:i:s", local_time())." ban4ip[".getmypid()."]: WARN [".$TARGET_CONF['target_service']."] Cannot send full msg by socket!? (".$TARGET_CONF['conf_file'].")"."\n";
@@ -48,7 +49,7 @@ function ban4ip_sendmsg($TARGET_CONF)
 // ----------------------------------------------------------------------
 // Sub Routine
 // ----------------------------------------------------------------------
-function ban4ip_mailsend($TARGET_CONF)
+function ban4ip_banmailsend($TARGET_CONF)
 {
     // メールタイトル初期化
     $MAIL_TITLE = $TARGET_CONF['mail_title']." [".$TARGET_CONF['target_service']."] Ban ".$TARGET_CONF['target_address'];
@@ -110,9 +111,12 @@ function ban4ip_mailsend($TARGET_CONF)
         }
     }
     
+    // 設定されている宛先にメール送信
     foreach($TARGET_CONF['mail_to'] as $MAIL_TO)
     {
-        $RESULT = mb_send_mail(
+///        $RESULT = mb_send_mail(
+        $RESULT = ban4ip_mail_send(
+            $TARGET_CONF,
             $MAIL_TO,
             $MAIL_TITLE,
             $MAIL_STR,
@@ -177,7 +181,7 @@ function ban4ip_ban($TARGET_CONF)
                 $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_after_ban');
                 
                 // BANした旨をメールで通知
-                ban4ip_mailsend($TARGET_CONF);
+                ban4ip_banmailsend($TARGET_CONF);
             }
             else
             {
@@ -220,7 +224,7 @@ function ban4ip_ban($TARGET_CONF)
                 $TARGET_CONF = ban4ip_exec($TARGET_CONF, 'exec_after_ban');
                 
                 // BANした旨をメールで通知
-                ban4ip_mailsend($TARGET_CONF);
+                ban4ip_banmailsend($TARGET_CONF);
             }
             else
             {
