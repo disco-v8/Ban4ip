9d980cce4e6cd174059faead2090f1181adcbd24
T.Kabu <kabu@fvg-on.net>
2019-09-05 10:18:57 +0900

    Update ban4ipd_sub.php

--- ban4ipd_sub.php
+++ ban4ipd_sub.php
@@ -759,7 +759,7 @@ function ban4ip_dbcheck($TARGET_CONF)
     if (!isset($TARGET_CONF['damage_recover']) || (isset($TARGET_CONF['damage_recover']) && $TARGET_CONF['damage_recover'] == 1))
     {
         // エラーの旨メッセージを設定
-        $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s", $TARGET_CONF['logtime'])." ban4ip[".getmypid()."]: WARN [".$TARGET_CONF['target_service']."] Cannot Query the DB, ".$TARGET_CONF['target_address']." ... DB File DELETE & REBOOT!)"."\n";
+        $TARGET_CONF['log_msg'] = date("Y-m-d H:i:s", $TARGET_CONF['logtime'])." ban4ip[".getmypid()."]: WARN [".$TARGET_CONF['target_service']."] Cannot Query the DB, ".$TARGET_CONF['target_address']." ... DB File DELETE & REBOOT!"."\n";
         // 親プロセスに送信
         ban4ip_sendmsg($TARGET_CONF);
         // カウント用データベースファイルを削除
